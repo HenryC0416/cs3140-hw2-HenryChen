7c6e84dc9e7db5ed9c9227cf9aeb494a4e1cf719
HenryC0416 <henrychen0416@gmail.com>
2026-02-04 13:15:22 -0500

    completed first part

---
+++ js-practice.js
@@ -0,0 +1,22 @@
+// Homework 2: JavaScript Practice
+// Name: Henry Chen
+// Date: 2/2/2026
+
+// ============ 1. Variables  ============
+
+const birthYear = 2004;
+let currAge = 21;
+let ageIn2030 = birthYear - 2030;
+
+// ============ 2. Data Types  ============
+
+
+// ============ 3. Arrays  ============
+
+
+// ============ 4. Objects  ============
+
+// ============ 5. Functions  ============
+
+
+// ============ 6. Bonus  ============
